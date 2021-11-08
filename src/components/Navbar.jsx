043fb3339892b1d14c5f48959dd4e2c52425dff0
SudhanshuBlaze <skrourkela@gmail.com>
2021-11-08 22:12:49 +0530

    Color correction in dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-300">
       <div className="flex justify-between items-center space-x-5 w-screen">
         <Link to="/">
-          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded-xl dark:bg-gray-500 dark:text-gray-900">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded-xl dark:bg-gray-700 dark:text-gray-300">
             Giggle ⚡
           </p>
         </Link>
         <button
           type="button"
           onClick={() => setDarkTheme(!darkTheme)}
-          className="text-xl bg-white dark:bg-gray-500 dark:text-gray-900 border-gray-500 border rounded-full px-2 py-1 hover:shadow-lg dark:hover:shadow-2xl "
+          className="text-xl bg-white dark:bg-gray-700 dark:text-gray-300 border-gray-500 border rounded-full px-2 py-1 hover:shadow-lg dark:hover:shadow-2xl "
         >
           {darkTheme ? "Dark 🌙" : "Light 💡"}
         </button>
